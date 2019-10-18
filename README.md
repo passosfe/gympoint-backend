c6d7dda91bf24e7b981e0d4be55116230a7961de
Felipe Passos <passos.fe@gmail.com>
2019-10-18 20:56:56 -0300

    added usercontroller && updated readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Gympoint
+# GYMPOINT - Backend
+
+## Descrição do Projeto
+
+O Gympoint é um app gerenciador de Academia. Com ele é possível gerenciar as contas e informações tanto dos usuários, quanto dos estudantes.
+
+O backend da aplicação está sendo desenvolvido em Node.js e utiliza o `express` como framework web e o `sequelize` para comunicação com o banco de dados PostgreSQL.
+
+### Rotas
+
+#### POST
+
+##### Inicio de Sessão
--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,16 @@ import { Router } from 'express';
 import SessionController from './app/controllers/SessionController';
 import StudentController from './app/controllers/StudentController';
 import UserController from './app/controllers/UserController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+
 routes.post('/students', StudentController.store);
 routes.put('/students', StudentController.update);
 
